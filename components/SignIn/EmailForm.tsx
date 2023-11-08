fc5322f7fb5afc1b110d244894cdf02abcb1f127
Martin Creedy <martin@openskydevelopment.co.uk>
2023-11-08 16:46:43 +0000

    chore: refactor sign-in

--- components/SignIn/EmailForm.tsx
+++ components/SignIn/EmailForm.tsx
@@ -1,50 +1,22 @@
 "use client"
 
-import { useState } from "react"
-import { useRouter } from "next/navigation"
-import { requestAccessCode } from "@/utils/api"
+import useEmailSignIn from "@/hooks/useEmailSignIn"
 import Button from "../Button"
 import Alert from "../Alert"
 import Loading from "../Loading"
 
 export default function EmailForm() {
-  const router = useRouter()
-  const [emailAddress, setEmailAddress] = useState<string>("")
-  const [loading, setLoading] = useState<boolean>(false)
-  const [error, setError] = useState<string | null>(null)
-  const [success, setSuccess] = useState<string | null>(null)
-
-  const submitForm = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-
-    try {
-      setLoading(true)
-      const result = await requestAccessCode(emailAddress)
-      if (result.outcome === "error") {
-        setError(result.data ?? "An error occurred")
-        setLoading(false)
-
-        setTimeout(() => {
-          setError(null)
-        }, 3000)
-        return
-      }
-
-      setSuccess("Check your email for the access code.  Redirecting...")
-      setLoading(true)
-
-      setTimeout(() => {
-        router.push(`/access-code?email=${emailAddress}`)
-      }, 1000)
-    } catch (error) {
-      alert("Something went wrong. Please try again.")
-      console.error("error", error)
-      return
-    }
-  }
+  const {
+    emailSignIn,
+    emailAddress,
+    setEmailAddress,
+    loading,
+    error,
+    success,
+  } = useEmailSignIn()
 
   return (
-    <form onSubmit={(e) => submitForm(e)} className="flex flex-col gap-2">
+    <form onSubmit={(e) => emailSignIn(e)} className="flex flex-col gap-2">
       {error && <Alert text={error ?? "Error"} type="error" />}
       {success && <Alert text={success ?? "Success"} type="success" />}
       <div className="flex justify-center">
