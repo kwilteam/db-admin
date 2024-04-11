02252fe20e16acb971d10dd2fc44146567f7bfd4
KwilLuke <105600466+KwilLuke@users.noreply.github.com>
2024-04-10 19:35:16 -0500

    console log wasm path

--- next.config.cjs
+++ next.config.cjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  experimental: {
-    nftTracing: true
-  }
 }
 
 module.exports = nextConfig
--- utils/server-actions.ts
+++ utils/server-actions.ts
@@ -4,6 +4,8 @@ import fs from "fs"
 import path from "path"
 import '../public/wasm/wasm_exec.js'
 import { CompiledKuneiform } from "@kwilteam/kwil-js/dist/core/payload"
+//@ts-ignore
+import wasm from "@/public/wasm/kl.wasm"
 
 interface IParseRes {
   json: string
@@ -24,10 +26,10 @@ export async function compileSchema(
   // 1. Load the Go runtime
   const go = new globalThis.Go()
   const wasmPath = path.join(process.cwd(), "public", "wasm", "kl.wasm")
+  console.log('WASM PATH', wasmPath)
   const wasm = fs.readFileSync(wasmPath)
   const wasmBuffer = Buffer.from(wasm)
   const typedArray = new Uint8Array(wasmBuffer)
-
   // 2. Instantiate the WebAssembly module
   const result: WebAssembly.WebAssemblyInstantiatedSource = await WebAssembly.instantiate(typedArray, go.importObject);
   go.run(result.instance)
