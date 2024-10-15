fe6468491a2d090f8934bf52bcc194ab6dfe0ccb
Ty D'Angelo <tydangelo18@gmail.com>
2024-10-15 15:09:48 -0500

    add in deployment expiration date to Deployment Cards.
    Signed-off-by: Ty D'Angelo <tydangelo18@gmail.com>

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -14,3 +14,25 @@ export const capitalize = (s: string) => {
   if (!s) return ""
   return s.charAt(0).toUpperCase() + s.slice(1).toLowerCase()
 }
+
+
+export const addDaysToTimestamp = (timestamp: number, days: number): string => {
+  const millisecondsInADay = 86400000; // 24 hours * 60 minutes * 60 seconds * 1000 milliseconds
+  const date = new Date(timestamp * 1000); // Convert to milliseconds
+  
+  // Add the specified number of days
+  date.setTime(date.getTime() + days * millisecondsInADay);
+  
+  return new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    timeZone: "America/Chicago",
+  }).format(date);
+}
+
+export const add15DaysToTimestamp = (timestamp: number): string => {
+  return addDaysToTimestamp(timestamp, 15);
+}
